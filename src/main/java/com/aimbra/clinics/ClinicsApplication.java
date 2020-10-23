d6f0e0692b67679d38c3c64adb5faf42c282f745
Thiago Cunha <thiago.santos.cunha@everis.com>
2020-10-23 00:45:25 -0300

    Projeto inicial

---
+++ src/main/java/com/aimbra/clinics/ClinicsApplication.java
@@ -0,0 +1,13 @@
+package com.aimbra.clinics;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClinicsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ClinicsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/aimbra/clinics/ClinicsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aimbra.clinics;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClinicsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
